0a6215fe2d758a66cd8e02fe77b197d10cdf3e34
Shrikant Manwatkar <148261027+shrikant21-97@users.noreply.github.com>
2025-11-04 22:33:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-aws-devops-azure-29"
+    bucket = "mydev-project-terraform-sample-batch-aws-devops-azure"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
