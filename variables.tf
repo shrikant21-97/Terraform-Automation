e9705f3d0311285cc3bb4734e401f04b6193a5c1
Shrikant Manwatkar <148261027+shrikant21-97@users.noreply.github.com>
2025-11-04 22:23:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "ap-south-1a" 
 }
 
 variable "key_name" { 
